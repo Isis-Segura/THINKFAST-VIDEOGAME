b5c1fd8fcca8038568d31e11d82e2668254da795
DanielCardenas-dev <jcardenas85@ucol.mx>
2025-09-22 13:38:56 -0600

    Ajuste

--- prueba.py
+++ prueba.py
@@ -22,47 +22,10 @@ clock = pygame.time.Clock()
 
 
 pygame.mouse.set_visible(1)
-"""
-coordenada del personaje
-coord_x = 10
-coord_y = 10
-velocidad
-x_speed = 0
-y_speed = 0
-"""
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-            
-        """
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_speed = -3
-            if event.key == pygame.K_RIGHT:
-                x_speed = 3
-            if event.key == pygame.K_UP:
-                y_speed = -3
-            if event.key == pygame.K_DOWN:
-                y_speed = 3
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                x_speed = 0
-            if event.key == pygame.K_RIGHT:
-                x_speed = 0
-            if event.key == pygame.K_UP:
-                y_speed = 0
-            if event.key == pygame.K_DOWN:
-                y_speed = 0
-        """
 
     screen.blit(imageb, [0, 0])
     screen.blit(titulob, [80, -50])
-    """
-    coord_x += x_speed
-    coord_y += y_speed
-    pygame.draw.rect(screen, red, (coord_x, coord_y, 100,100))
-    """
+
     pygame.display.flip()
     clock.tick(60)
 
